769aa2023d168c14cc7a034404f415ebb2b09c63
Arthur Bicalho <arthurbic95@gmail.com>
2022-02-10 00:14:09 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+
+import {
+  StyleSheet,
+  View,
+  Platform,
+  PermissionsAndroid,
+  Dimensions,
+} from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+
+const { width, height } = Dimensions.get("screen");
 
 export default function App() {
+  const [region, setRegion] = useState(null);
+
+  useEffect(() => {
+    getMyLocation();
+  }, []);
+
+  function getMyLocation() {
+      Location.getCurrentPositionAsync((info) => {
+      console.log("LAT", info.coords.latitude);
+      console.log("LONG", info.coords.longitude);
+
+      setRegion({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    },
+     () => {console.log('DEU ALGUM ERRO')}, {
+       enableHighAccurracy: true,
+       timeout: 2000
+     }
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        onMapReady={() => {
+          () =>
+            Platform.OS === "android"
+              ? PermissionsAndroid.request(
+                  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+                ).then(() => {
+                  console.log("USUARIO ACEITOU");
+                })
+              : "";
+        }}
+        style={{ width: width, height: height }}
+        region={region}
+        showsUserLocation
+        zoomEnabled={true}
+        minZoomLevel={17}
+        showsUserLocation={true}
+        loadingEnabled={true}
+      />
     </View>
   );
 }
@@ -13,8 +66,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
